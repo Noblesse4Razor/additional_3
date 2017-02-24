cc9689a268c7c871df67bf67c8e8567bc012d026
Noblesse4Razor <noblesse4razor@gmail.com>
2017-02-24 18:49:32 +0300

    MinBase/feature:straightforward implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function getBase(base) {
-  // your solution
+
+function DECtoANY(input, source) {
+
+    var result = "";
+    var base = +source;
+    do {
+        result += input%base;
+        input = Math.floor(input/base);
+    }while (input>0);
+
+    return result.split("").reverse().join("");
+}
+module.exports = function getBase(n) {
+    if(n.match(new RegExp("0+"))!==null&&n.match(new RegExp("0+"))[0].length-1) return n-1;
+   var i=2;
+   while (!DECtoANY(n,i).split("").every(x=>x=="1"))
+   {
+       i++;
+   }
+   return i;
 }
